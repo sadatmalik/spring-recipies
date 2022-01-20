758a2ce989d669bf4b8e01e90a2374bc1893a5ce
sadat.malik <sadat.malik@me.com>
2022-01-20 09:42:39 +0000

    index controller

---
+++ src/main/java/com/sadatmalik/spring/recipes/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.sadatmalik.spring.recipes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage() {
+        return "index";
+    }
+
+}
